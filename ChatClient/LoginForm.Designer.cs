ac7954bc20231f0c46ea51e2a9da65e64658ed86
Derpy Hooves <endusa.works@gmail.com>
2019-07-28 12:37:31 +0200

    Initial commit

---
+++ ChatClient/LoginForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ChatClient
+{
+	partial class LoginForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.lblUsername = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnLogin.Location = new System.Drawing.Point(65, 55);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(96, 23);
+			this.btnLogin.TabIndex = 0;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			this.btnLogin.Click += new System.EventHandler(this.LoginButton_Click);
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtUsername.Location = new System.Drawing.Point(15, 25);
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.Size = new System.Drawing.Size(203, 20);
+			this.txtUsername.TabIndex = 1;
+			// 
+			// lblUsername
+			// 
+			this.lblUsername.AutoSize = true;
+			this.lblUsername.Location = new System.Drawing.Point(12, 9);
+			this.lblUsername.Name = "lblUsername";
+			this.lblUsername.Size = new System.Drawing.Size(55, 13);
+			this.lblUsername.TabIndex = 2;
+			this.lblUsername.Text = "Username";
+			// 
+			// LoginForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(227, 90);
+			this.Controls.Add(this.lblUsername);
+			this.Controls.Add(this.txtUsername);
+			this.Controls.Add(this.btnLogin);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "LoginForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "BO Chat Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnLogin;
+		private System.Windows.Forms.TextBox txtUsername;
+		private System.Windows.Forms.Label lblUsername;
+	}
+}
---
+++ ChatClient/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ChatClient
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rtbChat = new System.Windows.Forms.RichTextBox();
+			this.txtMessage = new System.Windows.Forms.TextBox();
+			this.btnSend = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// rtbChat
+			// 
+			this.rtbChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.rtbChat.Location = new System.Drawing.Point(12, 12);
+			this.rtbChat.Name = "rtbChat";
+			this.rtbChat.Size = new System.Drawing.Size(761, 424);
+			this.rtbChat.TabIndex = 0;
+			this.rtbChat.Text = "";
+			// 
+			// txtMessage
+			// 
+			this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtMessage.Location = new System.Drawing.Point(12, 444);
+			this.txtMessage.Name = "txtMessage";
+			this.txtMessage.Size = new System.Drawing.Size(680, 20);
+			this.txtMessage.TabIndex = 1;
+			this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtMessage_KeyDown);
+			// 
+			// btnSend
+			// 
+			this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSend.Location = new System.Drawing.Point(698, 442);
+			this.btnSend.Name = "btnSend";
+			this.btnSend.Size = new System.Drawing.Size(75, 23);
+			this.btnSend.TabIndex = 2;
+			this.btnSend.Text = "Send";
+			this.btnSend.UseVisualStyleBackColor = true;
+			this.btnSend.Click += new System.EventHandler(this.SendButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(785, 477);
+			this.Controls.Add(this.btnSend);
+			this.Controls.Add(this.txtMessage);
+			this.Controls.Add(this.rtbChat);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "BO Chat";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox rtbChat;
+		private System.Windows.Forms.TextBox txtMessage;
+		private System.Windows.Forms.Button btnSend;
+	}
+}
+
